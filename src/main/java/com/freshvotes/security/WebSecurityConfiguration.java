5d70cfb100d5de67f1f960ead4898a6117c0c028
Sougata Roy <sougata.roy26@yahoo.com>
2022-04-23 21:47:33 +0530

    Added password encoder and Dashboard controller

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,16 +1,20 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("sr@email.com").password("123").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("sr@email.com").password(passwordEncoder().encode("123")).roles("USER");
     }
 
     @Override
@@ -23,5 +27,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/login").permitAll()
                 .and()
                 .logout().logoutUrl("/logout").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
